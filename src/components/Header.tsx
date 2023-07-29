86f7df2fee51143ab40475bfd9b55e0d2ad3a28c
alnwangchi <alnwangchi@gmail.com>
2023-07-29 17:52:45 +0800

    feat: add Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import mainLogo from '@/assets/img/main_logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaInstagram, FaSquareFacebook } from 'react-icons/fa6';
 import NavItem from './NavItem';
 
@@ -7,7 +8,9 @@ const Header = () => {
   return (
     <div className='bg-primary-yellow py-3'>
       <div className='container mx-auto flex justify-between'>
-        <Image src={mainLogo} width={300} height={115} alt='神龍變裝' />
+        <Link href='/'>
+          <Image src={mainLogo} width={300} height={115} alt='神龍變裝' />
+        </Link>
         <section className='space-y-2'>
           <div className='flex gap-2 text-black text-4xl cursor-pointer'>
             <a href=''>
@@ -20,8 +23,8 @@ const Header = () => {
           <div className='flex'>
             <input type='text' />
             <nav className='flex gap-2'>
-              <NavItem text='商品販售' />
-              <NavItem text='租借流程' />
+              <NavItem text='商品販售' href='/product-sale' />
+              <NavItem text='租借流程' href='/rental-process' />
             </nav>
           </div>
         </section>
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -5,15 +5,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface NavItemProps {
+  href: string;
   text: string;
   alt?: string;
 }
 
-const NavItem: FC<NavItemProps> = ({ text, alt }) => {
+const NavItem: FC<NavItemProps> = ({ href, text, alt }) => {
   return (
-    <Link href='/' className='relative'>
+    <Link href={href} className='relative'>
       <Image src={navItem_bg} width={150} height={60} alt='TBC' />
-      <div className='absolute top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 font-bold text-lg w-full flex justify-center'>
+      <div className='ab-center font-bold text-lg w-full flex justify-center'>
         <Image src={ball_icon} width={30} height={30} alt='TBC' />
         <span className='pr-2'>{text}</span>
       </div>
