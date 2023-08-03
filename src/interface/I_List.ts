69f8b533c0ea061fe1b396cecd1fa05205fee320
chunwen <hsbsbbdn@gmail.com>
2023-08-03 09:12:48 +0800

    feat: add interface

---
+++ src/interface/I_List.ts
@@ -0,0 +1,12 @@
+interface List {
+  category: string;
+  name: string;
+  title: string;
+  content: string;
+  price: number;
+  status: string;
+  situation: string;
+  image: string;
+}
+
+export type { List };
