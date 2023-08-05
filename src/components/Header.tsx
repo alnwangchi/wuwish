8cdce9e100b4d5a356f94da927f09b926f03760a
alnwangchi <alnwangchi@gmail.com>
2023-08-05 16:06:59 +0800

    feat: search input UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,54 +1,61 @@
-import mainLogo from "@/assets/img/main_logo.png";
-import Image from "next/image";
-import Link from "next/link";
-import { FaInstagram, FaSquareFacebook } from "react-icons/fa6";
-import NavItem from "./NavItem";
-import { usePathname, useRouter } from "next/navigation";
-
+import main_logo from '@/assets/img/main_logo.png';
+import icon_line from '@/assets/img/icon_line.png';
+import search_icon from '@/assets/img/search_icon.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaInstagram, FaSquareFacebook } from 'react-icons/fa6';
+import NavItem from './NavItem';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Header = () => {
   const pathname = usePathname();
   const isAdmin = pathname.startsWith('/dashboard');
 
   return isAdmin ? (
-    <div className="bg-primary-yellow py-3">
-      <div className="container mx-auto flex justify-between">
-        <section className="space-y-2">
-          <div className="flex">
-            <nav className="flex gap-2">
-              <NavItem text="商品上傳" href="/dashboard/upload" />
-              <NavItem text="商品列表" href="/dashboard/list" />
+    <div className='bg-primary-yellow py-3'>
+      <div className='container mx-auto flex justify-between'>
+        <section className='space-y-2'>
+          <div className='flex'>
+            <nav className='flex gap-2'>
+              <NavItem text='商品上傳' href='/dashboard/upload' />
+              <NavItem text='商品列表' href='/dashboard/list' />
             </nav>
           </div>
         </section>
       </div>
     </div>
-  ): (
-    <div className="bg-primary-yellow py-3">
-      <div className="container mx-auto flex justify-between">
-        <Link href="/">
-          <Image src={mainLogo} width={300} height={115} alt="神龍變裝" />
+  ) : (
+    <div className='bg-primary-yellow py-3'>
+      <div className='container mx-auto flex justify-between'>
+        <Link href='/'>
+          <Image src={main_logo} width={300} height={115} alt='神龍變裝' />
         </Link>
-        <section className="space-y-2">
-          <div className="flex gap-2 text-black text-4xl cursor-pointer">
-            <a href="">
+        <section className='flex flex-col justify-center px-10'>
+          <div className='pl-2 flex gap-2 text-black text-4xl cursor-pointer'>
+            <a href=''>
               <FaSquareFacebook />
             </a>
-            <a href="">
+            <a href=''>
               <FaInstagram />
             </a>
+            <a className='f-center' href=''>
+              <Image src={icon_line} width={32} height={32} alt='line' />
+            </a>
           </div>
-          <div className="flex">
-            <input type="text" />
-            <nav className="flex gap-2">
-              <NavItem text="商品販售" href="/product-sale" />
-              <NavItem text="租借流程" href="/rental-process" />
+          <div className='flex items-center gap-4'>
+            <div className='flex'>
+              <Image className='' src={search_icon} width={39} height={42} alt='line' />
+              <input type='text' className="font-cubic p-2 focus:outline-none placeholder:font-cubic"  placeholder='輸入文字 搜尋服裝' />
+            </div>
+            <nav className='flex gap-2'>
+              <NavItem text='商品販售' href='/product-sale' />
+              <NavItem text='租借流程' href='/rental-process' />
             </nav>
           </div>
         </section>
       </div>
     </div>
-  )
+  );
 };
 
-export default Header;
+export default Header; 
