594634ba67a7d6b4ed3bc1da80a94544922cf462
chunwen <hsbsbbdn@gmail.com>
2023-08-07 23:45:11 +0800

    feat: adjust upload schema and validate rule

--- src/app/dashboard/upload/page.tsx
+++ src/app/dashboard/upload/page.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { FC } from 'react';
+import React, { useEffect } from "react";
 
 import { tmpCategory } from "@/constance";
-import { useForm,SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import "./style.scss";
@@ -17,45 +17,56 @@ const uploadSchema = yup.object({
   price: yup.number(),
   situation: yup.string(),
   status: yup.string(),
-  image: yup.string().required()
-}).required();
+  image: yup.mixed().required(),
+});
 
 type FormValues = {
-  category: string
-  name: string
-  title: string
-  content?: string
-  price?: number
-  situation?:string
-  status?:string
-  image: string
-}
-
+  category: string;
+  name: string;
+  title: string;
+  content?: string;
+  price?: number;
+  situation?: string;
+  status?: string;
+  image: unknown;
+};
 
 const UploadPage = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    setError,
+    clearErrors,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(uploadSchema),
+    resolver: yupResolver(uploadSchema)
   });
-  const [file, setFile] = useState('');
-  
+  const [file, setFile] = useState("");
+
+  useEffect(() => {
+    setError("image", {
+      type: "custom",
+      message: "請上傳圖片！",
+    })
+  },[setError])
+
   const handleUploadImage = (e: any) => {
-      setFile(URL.createObjectURL(e.target.files[0]));
-  }
+    setFile(URL.createObjectURL(e.target.files[0]));
+    clearErrors(["image"]);
+  };
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data);
+    console.log('1', data);
     // send api
-  }
+  };
 
   return (
     <div className="container grid grid-cols-2 gap-4 p-4">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label><span>*</span>類別</label>
+          <label>
+            <span>*</span>類別
+          </label>
           <select {...register("category")}>
             {tmpCategory.map((c) => (
               <option key={c.en} value={c.en}>
@@ -66,13 +77,17 @@ const UploadPage = () => {
         </div>
 
         <div>
-        <label><span>*</span>劇名</label>
+          <label>
+            <span>*</span>劇名
+          </label>
           <input {...register("name")} />
           {errors.name && <p>劇名是必填欄位</p>}
         </div>
 
         <div>
-          <label><span>*</span>名稱</label>
+          <label>
+            <span>*</span>名稱
+          </label>
           <input {...register("title")} />
           {errors.title && <p>名稱是必填欄位</p>}
         </div>
@@ -85,11 +100,8 @@ const UploadPage = () => {
 
         <div>
           <label>價格</label>
-          <input
-            type="number"
-            {...(register("price"), { valueAsNumber: true })}
-          />
-          {errors.price && <p>{errors.price.message}</p>}
+          <input type="number" {...register("price")}  defaultValue={0} />
+          {errors.price && <p>價格只能輸入數字</p>}
         </div>
 
         <div>
@@ -105,19 +117,26 @@ const UploadPage = () => {
         </div>
 
         <div>
-            <label><span>*</span>上傳圖片</label>
-            <input type="file" accept="image/*" {...(register("image"))}/>
-            {errors.image && <p>請上傳圖片</p>}
+          <label>
+            <span>*</span>上傳圖片
+          </label>
+          <input
+            type="file"
+            accept=".jpg, .jpeg, .png, .gif, .webp, .svg, .bmp"
+            {...register("image")}
+            onChange={handleUploadImage}
+          />
+          {errors.image?.type === "custom" && <p>{errors.image.message}</p>}
         </div>
 
         <input type="submit" />
       </form>
       <div className="border border-indigo-600">
-       <label>預覽圖片</label>
-       {file && <img src={file} className="" alt="image" />}
+        <label>預覽圖片</label>
+        {file && <img src={file} className="" alt="image" />}
       </div>
     </div>
   );
-}
+};
 
 export default UploadPage;
