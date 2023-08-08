d2640112f085dd216b5bd0689d704306b1d8d8f7
chunwen <hsbsbbdn@gmail.com>
2023-08-08 08:31:22 +0800

    feat: adjust admin header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+'use client';
 import main_logo from '@/assets/img/main_logo.png';
 import icon_line from '@/assets/img/icon_line.png';
 import search_icon from '@/assets/img/search_icon.png';
@@ -5,17 +6,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FaInstagram, FaSquareFacebook } from 'react-icons/fa6';
 import NavItem from './NavItem';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import useAuthenticate from '@/hooks/useAuthenticate';
 
 const Header = () => {
   const pathname = usePathname();
-  const isAdmin = pathname.startsWith('/dashboard');
+  const isAdmin =
+    pathname.startsWith('/dashboard') || pathname.startsWith('/login');
 
   return isAdmin ? (
     <div className='bg-primary-yellow py-3'>
       <div className='container mx-auto flex justify-between'>
-        <section className='space-y-2'>
-          <div className='flex'>
+        <Link href='/'>
+          <Image src={main_logo} width={300} height={115} alt='神龍變裝' />
+        </Link>
+        <section className='flex flex-col justify-center px-10'>
+          <div className='flex items-center'>
             <nav className='flex gap-2'>
               <NavItem text='商品上傳' href='/dashboard/upload' />
               <NavItem text='商品列表' href='/dashboard/list' />
@@ -44,8 +50,18 @@ const Header = () => {
           </div>
           <div className='flex items-center gap-4'>
             <div className='flex'>
-              <Image className='' src={search_icon} width={39} height={42} alt='line' />
-              <input type='text' className="font-cubic p-2 focus:outline-none placeholder:font-cubic"  placeholder='輸入文字 搜尋服裝' />
+              <Image
+                className=''
+                src={search_icon}
+                width={39}
+                height={42}
+                alt='line'
+              />
+              <input
+                type='text'
+                className='font-cubic p-2 focus:outline-none placeholder:font-cubic'
+                placeholder='輸入文字 搜尋服裝'
+              />
             </div>
             <nav className='flex gap-2'>
               <NavItem text='商品販售' href='/product-sale' />
@@ -58,4 +74,4 @@ const Header = () => {
   );
 };
 
-export default Header; 
+export default Header;
