a9fbc0a51c66fe5074873a7b4e59989d91c6ebd5
chunwen <hsbsbbdn@gmail.com>
2023-08-08 08:31:48 +0800

    feat: add authenticate hooks

---
+++ src/hooks/useAuthenticate.ts
@@ -0,0 +1,30 @@
+'use client';
+import { authorization } from '@/constance';
+import { getCookie, setCookie } from 'cookies-next';
+
+export const authenticationService = (account: string, password: string) => {
+  if (
+    account === authorization.account &&
+    password === authorization.password
+  ) {
+    setCookie('account', authorization.account);
+    setCookie('password', authorization.password);
+    return true;
+  }
+  return false;
+};
+
+const useAuthenticate = () => {
+  if (
+    authenticationService(
+      (getCookie('account') as string) || '',
+      (getCookie('password') as string) || ''
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default useAuthenticate;
