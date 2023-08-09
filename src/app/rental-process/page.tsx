dd4f6b0f20f6281d6548a45ef2e041a27ea59381
alnwangchi <alnwangchi@gmail.com>
2023-08-09 21:28:06 +0800

    fix: rental text too small

--- src/app/rental-process/page.tsx
+++ src/app/rental-process/page.tsx
@@ -71,8 +71,8 @@ const page = () => {
         </div>
         <div className='relative'>
           <Image src={rental_bg} className='w-full' alt='deco' />
-          <div className='ab-center w-3/4 font-cubic leading-loose text-white'>
-            <h2 className='text-center mb-4'>租借細則</h2>
+          <div className='ab-center w-3/4 font-cubic leading-loose text-white text-[2vw]'>
+            <h2 className='text-center mb-10 text-[4vw]'>租借細則</h2>
             <p>
               1.訂單確認後，每套服裝需先預繳1/2之訂金，
               <span className='text-red-600'>如訂單取消，訂金不退還。</span>
