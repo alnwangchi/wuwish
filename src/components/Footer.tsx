6583b4bdd7965249fa8546ccfae199cb3e391970
alnwangchi <alnwangchi@gmail.com>
2023-08-12 20:29:45 +0800

    feat: Footer RWD

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,24 +6,24 @@ import footer_line from '@/assets/img/footer_line.png';
 
 const Footer = () => {
   return (
-    <div className='bg-primary-yellow pb-5 font-cubic mt-10'>
-      <div className='relative container mx-auto flex justify-center items-center'>
-        <div className='text-center text-primary-dk'>
-          <Image src={mainLogo} width={300} height={115} alt='神龍變裝' className='block' />
+    <div className="bg-primary-yellow pb-5 font-cubic mt-10">
+      <div className="relative container mx-auto flex justify-center items-center flex-col gap-3 sm:flex-row">
+        <div className="text-center text-primary-dk">
+          <Image src={mainLogo} width={300} height={115} alt="神龍變裝" className="block" />
           <p>台北市漢中街150號2樓 </p>
           <p>0968-270178</p>
           <p>wu.wish88@gmail.com</p>
         </div>
 
-        <div className='flex gap-3 absolute bottom-0 left-0'>
-          <a href='/'>
-            <Image src={footer_fb} width={75} height={105} alt='神龍變裝 facebook' />
+        <div className="flex gap-3 sm:absolute sm:bottom-0 sm:left-0">
+          <a href="/">
+            <Image src={footer_fb} width={75} height={105} alt="神龍變裝 facebook" />
           </a>
-          <a href='/'>
-            <Image src={footer_ig} width={75} height={105} alt='神龍變裝 instagram' />
+          <a href="/">
+            <Image src={footer_ig} width={75} height={105} alt="神龍變裝 instagram" />
           </a>
-          <a href='/'>
-            <Image src={footer_line} width={75} height={105} alt='神龍變裝 line' />
+          <a href="/">
+            <Image src={footer_line} width={75} height={105} alt="神龍變裝 line" />
           </a>
         </div>
       </div>
