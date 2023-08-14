0d21ce25269c481c4b6553c920d1d701c5dd9609
chunwen <hsbsbbdn@gmail.com>
2023-08-15 00:13:59 +0800

    feat: axios instance update

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,32 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'https://some-domain.com/api/',
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: 'http://localhost:9527/',
+  timeout: 3000,
 });
+AxiosInstance.defaults.headers.get['Content-Type'] = 'application/json';
+AxiosInstance.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+
+
+// Add a request interceptor to set up retry configuration
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor to handle retries
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default AxiosInstance;
