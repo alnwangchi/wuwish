812c00f80612c617ca6bdf895448095e52622c55
chunwen <hsbsbbdn@gmail.com>
2023-08-16 01:41:04 +0800

    feat: add api response interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,8 +1,4 @@
-import {
-  ControllerFieldState,
-  ControllerRenderProps,
-  UseFormStateReturn,
-} from 'react-hook-form';
+import { ControllerFieldState, ControllerRenderProps, UseFormStateReturn } from 'react-hook-form';
 
 interface ReactFormProps {
   field: ControllerRenderProps<any, any>;
@@ -10,4 +6,40 @@ interface ReactFormProps {
   fieldState: ControllerFieldState;
 }
 
-export type { ReactFormProps };
+interface TableResponse {
+  results: ProductInfo[];
+  total_count: number;
+}
+
+interface ProductInfo {
+  image_id: string;
+  image_path: string;
+  info: ProductDetail;
+}
+
+interface ProductDetail {
+  business_type: BusinessType;
+  category: string;
+  content: string;
+  name: string;
+  number: Number;
+  price: string;
+  status: string;
+  title: string;
+}
+
+interface QueryTableParams{
+  title?: string;
+  name?: string;
+  page_number: number;
+  page_size: number;
+  number?: string;
+  business_type: BusinessType
+}
+
+export enum BusinessType {
+  Rent = 'rent',
+  Sell = 'sell'
+}
+
+export type { ReactFormProps, TableResponse, ProductInfo,QueryTableParams };
