15e7589122da246d57bbcd857d09b11a41084bc9
chunwen <hsbsbbdn@gmail.com>
2023-08-18 12:17:59 +0800

    feat: update button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,16 @@
 import { Button as ButtonAntd } from 'antd';
 import React from 'react';
 
-const Button = ({ text = '提交' }: { text: string }) => (
-  <ButtonAntd className="flex justify-center align-middle btn">{text}</ButtonAntd>
+interface ButtonProps {
+  text: string;
+  customClass?: string;
+  htmlType?: "button" | "submit" | "reset"
+}
+ 
+const Button = ({ text="提交",htmlType="submit", customClass ='' }: ButtonProps) => (
+  <ButtonAntd htmlType={htmlType} className={`flex justify-center align-middle btn ${customClass}`} >
+    {text}
+  </ButtonAntd>
 );
 
 export default Button;
