1d505ab5d48ab9b5c21afa3616479c84433339ed
chunwen <hsbsbbdn@gmail.com>
2023-08-20 13:34:32 +0800

    feat: adjust dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import useAuthenticate from '@/hooks/useAuthenticate';
+import { Spin } from 'antd';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -14,7 +15,12 @@ const Dashboard = () => {
     }
   }, [isCanViewAdmin, router]);
 
-  return <></>;
+  return (
+    <div className='flex flex-col gap-4 items-center'>
+      <Spin size='large'></Spin>
+      <p className='text-white'>身份驗證中....</p>
+    </div>
+  );
 };
 
 export default Dashboard;
