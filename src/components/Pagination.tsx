a8f4ad79be99b2d9fdb590b977bf06ac3529d527
alnwangchi <alnwangchi@gmail.com>
2023-09-03 16:35:12 +0800

    fix: handle jump 5 page button

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,7 +5,7 @@ import { Pagination as AntdPagination } from 'antd';
 import clsx from 'clsx';
 import Image from 'next/image';
 import pagination_bg from '@/assets/img/pagination_bg.png';
-
+import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from 'react-icons/md';
 interface PaginationPropsCustomize {
   current: number;
   total?: number;
@@ -19,8 +19,30 @@ const Pagination: FC<PaginationPropsCustomize> = ({ current, total, onChange }) 
       onChange={onChange}
       total={total}
       pageSize={25}
+      showSizeChanger={false}
       itemRender={(page, type) => {
-        // type: 'page' | 'prev' | 'next' 暫只處理 page
+        // type: 'page' | 'prev' | 'next' | 'jump-prev'| 'jump-next'
+        if (type === 'jump-prev') {
+          return (
+            <div className="relative">
+              <Image src={pagination_bg} width={53} height={61} alt="pagination" />
+              <p className="ab-center font-timer font-bold text-xl">
+                <MdKeyboardDoubleArrowLeft />
+              </p>
+            </div>
+          );
+        }
+        if (type === 'jump-next') {
+          return (
+            <div className="relative">
+              <Image src={pagination_bg} width={53} height={61} alt="pagination" />
+              <p className="ab-center font-timer font-bold text-xl">
+                <MdKeyboardDoubleArrowRight />
+              </p>
+            </div>
+          );
+        }
+        // 尚不處理上下一頁按鈕
         if (type !== 'page') return null;
         return (
           <div className="relative">
