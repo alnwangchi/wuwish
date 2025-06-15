8dd2e56db16214cfd48aa9a41a59cb739d0b1d7b
alnwangchi <alnwangchi@gmail.com>
2025-06-15 15:42:49 +0800

    add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import './globals.css';
-import './form.css';
-import type { Metadata } from 'next';
-import RootStyleRegistry from '@/components/RootStyleRegistry';
-import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Script from 'next/script';
+import Header from '@/components/Header';
+import RootStyleRegistry from '@/components/RootStyleRegistry';
+import Analytics from '@/ga/Analytics';
+import type { Metadata } from 'next';
+import './form.css';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: '神龍變裝 common title',
@@ -15,24 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-JFG2SEKK04"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-JFG2SEKK04', {
-                page_path: window.location.pathname,
-              });
-            `
-          }}
-        />
+        <Analytics />
       </head>
       <body>
         <RootStyleRegistry>
--- src/ga/Analytics.tsx
+++ src/ga/Analytics.tsx
@@ -1,5 +1,28 @@
 'use client';
 
+import Script from 'next/script';
+
 export default function Analytics() {
-  return null;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JFG2SEKK04"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JFG2SEKK04', {
+                page_path: window.location.pathname,
+              });
+            `
+        }}
+      />
+    </>
+  );
 }
